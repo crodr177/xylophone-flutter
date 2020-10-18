0fed34007a9f80e9b6e001c25a92d9945029e1bf
Cruz Rodriguez <cruzrodriguez1127@gmail.com>
2020-10-17 19:16:08 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Xylophone 🎹
+
+Music app that plays Xylophone sounds. 
+
+![Finished App](https://github.com/londonappbrewery/Images/blob/master/xylophone-flutter.png)
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNum) {
+    final player = AudioCache();
+    player.play('note$soundNum.wav');
+  }
+
+  Expanded buildKey(int soundNum, Color keyColor) {
+    return Expanded(
+      child: FlatButton(
+        onPressed: () {
+          playSound(soundNum);
+        },
+        child: Text(''),
+        color: keyColor,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(1, Colors.red),
+              buildKey(2, Colors.orange),
+              buildKey(3, Colors.yellow),
+              buildKey(4, Colors.green),
+              buildKey(5, Colors.teal),
+              buildKey(6, Colors.blue),
+              buildKey(7, Colors.purple),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
